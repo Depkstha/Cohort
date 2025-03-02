c401007ddbe7ace66774c0c86cf0feb908a672c6
Depkstha <dharmarajshrestha9870@gmail.com>
2025-03-02 23:41:12 +0545

    Add JavaScript data types notes and enhance color generation functionality

---
+++ Notes/js/01_datatype.js
@@ -0,0 +1,80 @@
+//Javascript - When first it was initially created - it had another name Livescript.
+//It is fully independent language with its own specification call ECMAScript.
+//ECMAScript - It is a standard for scripting language including JavaScript, JScript and Action Script
+
+// Scripting language is a type of programming language where instructions are interpreted line by line at runtime, meaning the code is executed directly without the needing to be compiled beforehand
+
+//Eight basic data types
+
+//Number
+let number = 123456;
+console.log(typeof number);
+
+//Besides regular number there are so callled "special numeric values" which also belongs to Number data type
+//Infinity, -Infinity and NaN // typeof these are number
+
+console.log(typeof -Infinity, typeof NaN, typeof Infinity);
+
+//Infinity represents the mathematical Infinity. It is a special value that's greater than any number
+//We can get it as a result of divison by zero or just referencing it directly.
+
+console.log(1/0);
+console.log(Infinity);
+
+//NaN represents computational error. It is a result of an incorrect or an undefined mathematical operation for instance.
+console.log("not a number" / 2);
+
+//If there's a NaN somewhere in a mathematical expression, it propagates to the whole result to NaN except NaN ** 0 is 1;
+console.log(NaN ** 0); //exponential
+
+//String 
+//There are three types of quotes
+let message = "hello";
+let message1 = 'Hello one';
+let message2 = `Hello two`;
+
+console.log(typeof message);
+
+//no character type for single character only one string.
+
+//Boolean
+// Has only two values: true or false
+
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+console.log(typeof nameFieldChecked);
+
+// also come as result of comparison
+let age = 25;
+console.log(age > 18);
+
+//Null
+//The special value does not belong to any of the types described above
+//Null is just a special value which represents "nothing, "empty" or "value unknown";
+
+let name = null;
+console.log(name);
+console.log(typeof name);
+
+//Undefined 
+//It means value is not assigned. If a variable is declared but not assigned then its value is undefined.
+
+let value;
+console.log(value);
+console.log(typeof value);
+
+//Technically it is possible to explicitly assign undefined to a variable 
+let lname = "Shrestha";
+lname = undefined;
+console.log(lname);
+
+//But it is not recommended. It is prefer to use null to assign an empty or unknown value to a variable when undefined is reserved as a default initial value for unassigned things.
+
+//All above data type are called primitive data type and object is non-primitive
+
+//Objects and Symbols
+//Object are non primitive data type used to store collection of data and more complex entities in key value pair.
+
+console.log(4 / NaN ** 0 + 2);
+ 
--- closure-assignment/script.js
+++ closure-assignment/script.js
@@ -1,6 +1,11 @@
 const colorHexPattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+const whiteColorHexPattern = /^#([fF]{3}|[fF]{6})$/;
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 const colorInput = document.querySelector(".color-input");
 const createBtn = document.querySelector(".create-btn");
+const generateColorBtn = document.querySelector(".generate-color-btn");
 const body = document.querySelector("body");
 const buttons = document.querySelector(".buttons");
 
@@ -10,9 +15,11 @@ createBtn.addEventListener("click", function (e) {
   const button = document.createElement("button");
 
   if (!colorHex || !colorHexPattern.test(colorHex)) {
-    alert("Enter correct color hex value!");
+    alert("Enter correct color hex value 6 or 3!");
     return;
   }
+
+  if (whiteColorHexPattern.test(colorHex)) button.style.color = "#000000";
 
   button.classList.add("color-btn");
   button.style.backgroundColor = colorHex;
@@ -26,3 +33,10 @@ createBtn.addEventListener("click", function (e) {
     body.style.backgroundColor = colorHex;
   });
 });
+
+generateColorBtn.addEventListener("click", function () {
+  const hexCode = "#" + Array.from({ length: 6 }, () => hex[generateRandomNumber()]).join("");
+  colorInput.value = hexCode;
+});
+
+const generateRandomNumber = () => Math.floor(Math.random() * hex.length);
