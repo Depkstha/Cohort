662f283678f92b35df4a9b6dab253c86571bfe66
Depkstha <dharmarajshrestha9870@gmail.com>
2025-03-28 18:44:42 +0545

    final updated

--- Masterji/library/script.js
+++ Masterji/library/script.js
@@ -7,6 +7,7 @@ const sortSelect = document.getElementById("sort-select");
 const searchInput = document.getElementById("search-input");
 const BOOKS_PER_PAGE = 10;
 let currentPage = 1;
+let hasGridLayout = true;
 
 // functionality to toggle list and grid view
 gridButtons.forEach((button) => {
@@ -20,8 +21,14 @@ gridButtons.forEach((button) => {
     button.classList.add("bg-amber-400/20", "border-amber-400/30");
 
     bookContainer.classList.toggle("grid-cols-1");
-    bookContainer.classList.toggle("lg:grid-cols-3");
-    bookContainer.classList.toggle("sm:grid-cols-2");
+    bookContainer.classList.toggle("lg:grid-cols-1");
+    bookContainer.classList.toggle("sm:grid-cols-1");
+
+    bookContainer.querySelectorAll(".book-card").forEach((element) => {
+      element.classList.toggle("flex");
+      element.classList.toggle("p-4");
+    });
+    hasGridLayout = !hasGridLayout;
   });
 });
 
@@ -36,7 +43,6 @@ const fetchBooks = async (page = 1, query, limit = BOOKS_PER_PAGE) => {
   if (query) params.set("query", query);
 
   API_URL.search = params.toString();
-
 
   const options = { method: "GET", headers: { accept: "application/json" } };
 
@@ -65,23 +71,25 @@ const renderBookCards = (books) => {
           infoLink,
         },
       }) => `
-    <div class="group bg-gray-800/80 backdrop-blur-sm rounded-xl border border-white/10 overflow-hidden transition-all duration-300 hover:border-amber-400/30 hover:-translate-y-1">
-      <a target="_blank" href="${infoLink}">
-      <div class="aspect-[5/4] bg-gray-700 relative overflow-hidden">
-        <img src="${thumbnail}"
-             alt="${title}"
-             class="w-full h-full object-contain transition-transform duration-500 group-hover:scale-105">
-        <div class="absolute inset-0 bg-gradient-to-t from-gray-900/60 to-transparent"></div>
-      </div>
-      <div class="p-4">
+    <div class="book-card ${
+      !hasGridLayout && "flex p-4"
+    } group bg-gray-800/80 backdrop-blur-sm rounded-xl border border-white/10 overflow-hidden transition-all duration-300 hover:border-amber-400/30 hover:-translate-y-1">
+    <div class="aspect-[5/4] bg-gray-700 relative overflow-hidden rounded-md">
+    <img src="${thumbnail}"
+    alt="${title}"
+    class="w-full h-full object-contain transition-transform duration-500 group-hover:scale-105">
+    <div class="absolute inset-0 bg-gradient-to-t from-gray-900/60 to-transparent"></div>
+    </div>
+    <div class="p-4">
+    <a target="_blank" href="${infoLink}">
         <h3 class="font-semibold text-lg mb-2 text-gray-100">${title}</h3>
         <p class="text-gray-400 mb-2">${authors.join(",")}</p>
         <div class="flex justify-between items-center text-sm text-gray-500">
           <span>${publisher}</span>
           <span>${publishedDate}</span>
         </div>
+        </a>
       </div>
-      </a>
     </div>
   `
     )
@@ -106,13 +114,21 @@ const renderPagination = ({
   const html = `
         <div class="flex flex-col items-center mt-5">
   <span class="text-sm text-gray-700 dark:text-gray-400">
-      Showing <span class="font-semibold text-gray-900 dark:text-white">${previousPageIndex * limit + 1}</span> to <span class="font-semibold text-gray-900 dark:text-white">${previousPageIndex * limit + currentPageItems}</span> of <span class="font-semibold text-gray-900 dark:text-white">${totalItems}</span> Entries
+      Showing <span class="font-semibold text-gray-900 dark:text-white">${
+        previousPageIndex * limit + 1
+      }</span> to <span class="font-semibold text-gray-900 dark:text-white">${
+    previousPageIndex * limit + currentPageItems
+  }</span> of <span class="font-semibold text-gray-900 dark:text-white">${totalItems}</span> Entries
   </span>
   <div class="pagination inline-flex mt-2 xs:mt-0">
-      <button ${!previousPage && "disabled"} class="prev-button flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 rounded-s hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+      <button ${
+        !previousPage && "disabled"
+      } class="prev-button flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 rounded-s hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           Prev
       </button>
-      <button  ${!nextPage && "disabled"} class="next-button flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+      <button  ${
+        !nextPage && "disabled"
+      } class="next-button flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           Next
       </button>
   </div>
@@ -139,7 +155,10 @@ const showBookCards = async (page, query, sort) => {
   let books = response?.data?.data;
 
   if (!books || books.length === 0) {
-    bookContainer.innerHTML = `<p class="text-white/60 text-center">No books found</p>`;
+    bookContainer.innerHTML = `<div class="text-center"><p class="text-white/60">No books found</p></div>`;
+
+    //remove pagination
+    bookContainer.nextElementSibling.remove();
     return;
   }
 
@@ -168,10 +187,13 @@ const showBookCards = async (page, query, sort) => {
 };
 
 // Event listeners
-searchInput.addEventListener("keyup", (e) => showBookCards(currentPage, e.target.value, sortSelect.value));
+searchInput.addEventListener("keyup", (e) =>
+  showBookCards(currentPage, e.target.value, sortSelect.value)
+);
 
-sortSelect.addEventListener("change", (e) => showBookCards(currentPage, searchInput.value, e.target.value));
-
+sortSelect.addEventListener("change", (e) =>
+  showBookCards(currentPage, searchInput.value, e.target.value)
+);
 
 document.addEventListener("DOMContentLoaded", () => {
   showBookCards();
